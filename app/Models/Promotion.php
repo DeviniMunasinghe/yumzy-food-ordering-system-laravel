cae502252ff0deea65c15070089da2e70bc69dd3
Devini Munasinghe <devini24@stu.codepark.university>
2025-01-02 16:18:29 +0530

    add Promotion model with relationships and fillable attributes

---
+++ app/Models/Promotion.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Promotion extends Model
+{
+    use HasFactory;
+
+    protected $fillable=[
+        'title',
+        'promotion_description',
+        'promotion_image',
+        'start_date',
+        'end_date',
+        'categories',
+    ];
+
+    public function rules(){
+        return $this->hasMany((PromotionRule::class));
+    }
+}
